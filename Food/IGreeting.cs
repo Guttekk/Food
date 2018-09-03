00ef5cfede8b83ad58a5d3a23a8e6db61dc5bb4a
Rafał Gucwa <rafal.gucwa@rockwool.com>
2018-09-03 14:12:59 +0200

    Add project files.

---
+++ Food/IGreeting.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Food
+{
+    public interface IGreeting
+    {
+        string GreetingOfTheDay();
+    }
+
+    public class Greeting : IGreeting
+    {
+        private IConfiguration _configuration;
+
+        public Greeting(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GreetingOfTheDay()
+        {
+            return _configuration["Greeting"];
+        }
+    }
+}
